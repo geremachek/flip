b68ca6e23ba5115387aea244a934dcadc0c15f31
freyja <mrender2005@gmail.com>
2023-08-02 16:43:29 -0700

    improve addStr function

--- slidedeck/draw.go
+++ slidedeck/draw.go
@@ -5,10 +5,7 @@ import "github.com/gdamore/tcell"
 // add text to the screen
 
 func addString(s tcell.Screen, style tcell.Style, x int, y int, text string) {
-	curs := x
-
-	for _, ch := range text {
-		s.SetContent(curs, y, ch, []rune{}, style)
-		curs++
+	for i, ch := range text {
+		s.SetContent(i+x, y, ch, []rune{}, style)
 	}
 }
